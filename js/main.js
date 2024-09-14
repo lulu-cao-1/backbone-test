938091b7f75dae2f156be2da253eaa67aeab0ace
lulu-cao-1 <lcao@backstitch.io>
2024-09-13 23:19:44 -0500

    pass models and collections to views

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ var Song = Backbone.Model.extend({
   urlRoot: "/api/songs",
   defaults: {
     genre: "Country",
+    listeners: 0
   },
   validate: function(attrs){
     if (!attrs.title) {
@@ -60,11 +61,8 @@ songs.add(new Song({ songId: 5, title: "Waiting for Autumn", artist: "Orange", d
 songs.add(new Song({ songId: 6, title: "Well, That's Climate Change for Ya", artist: "Orange", downloads: 70 }), { at: 0 }); // Adding a song at a specific index
 songs.push(new Song({ songId: 7, title: "I'm Going to Michigan Tomorrow" })); // Adding a song at the end of the collection
 
-console.log('All songs in the collection', songs);
-console.log('All songs in the collection', songs.toJSON());
 console.log('The song with cid c4', songs.get("c4")); // Getting a song by cid
 songs.remove(songs.get(3));
-console.log('Removed the song with id 3');
 console.log('The first song in the collection', songs.at(0)); // Getting a song by index
 console.log('The song with id 7', songs.get(7)); // Getting a song by id
 console.log('Removed the last song', songs.pop());
@@ -77,23 +75,61 @@ songs.each(function(song) {
   console.log(song.toJSON());
 });
 
-// Creating a new view class
-var SongView = Backbone.View.extend({
-  tagName: "span",
+// Creating a new view class and passing a model to it
+var SongView = Backbone.View.extend({ // Creating an element that is a span with the class, id, and data-genre attributes
+  initialize: function() {
+    this.model.on("change", this.render, this); // event name, callback, context
+  },
+  events: {
+    "click": "onClick",
+    "click .bookmark": "onClickBookmark"
+  },
+  onClick: function() {
+    this.model.set("listeners", this.model.get("listeners") + 1); // backbone model stores attributes as a hash, so we need to use get and set to access and modify the model
+  },
+  onClickBookmark: function(e) {
+    e.stopPropagation();
+    console.log("Bookmark button clicked.");
+  },
+  tagName: "li",
   className: "song",
-  id: "1234",
   attributes: {
     "data-genre": "Country"
   },
   render: function() {
-    this.$el.html("heyo good night");
+    this.$el.html(this.model.get("title") + ` - Listeners: ${this.model.get("listeners")} <button>Listen</button> <button class='bookmark'>Bookmark</button>`); // $el is a jQuery object that wraps the view's element
+    this.$el.attr("id", this.model.get("songId"));
     return this;
   }
 });
 
-// var songView = new SongView({ el: "#container" });
-// songView.render();
+// Creating a new view class and passing a collection to it
+var SongsView = Backbone.View.extend({
+  tagName: "ul",
+  initialize: function() {
+    this.model.on("add", this.onSongAdded, this);
+    this.model.on("remove", this.onSongRemoved, this);
+  },
+  onSongAdded: function(song) {
+    var songView = new SongView({ model: song });
+    this.$el.append(songView.render().$el);
+  },
+  onSongRemoved: function(song) {
+    // this.$el.find("li#" + song.get("songId")).remove();
+    this.$("li#" + song.get("songId")).remove(); // This also works
+  },
+  render: function() {
+    var self = this; // To access the view object inside the each function
+    this.model.each(function(song) {
+      var songView = new SongView({ model: song });
+      self.$el.append(songView.render().$el); // Appending the view's element to the parent view's element
+    })
+  }});
 
-var songView2 = new SongView();
-songView2.render();
-$("#container").html(songView2.$el);
+// Inserting the view's element into the DOM; if #song isn't found, the jQuery object will be created but it won't be inserted into the DOM
+var songView = new SongView({ el: "#song", model: song });
+songView.render();
+
+var songsView = new SongsView({ model: songs });
+songsView.render();
+$("#songs").html(songsView.$el);
